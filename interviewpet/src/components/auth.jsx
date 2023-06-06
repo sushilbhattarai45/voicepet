4dbfccb671b4ba20faa07b876898de1622c1a3c4
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2023-06-06 23:26:19 +0545

    home only remain

--- interviewpet/src/components/auth.jsx
+++ interviewpet/src/components/auth.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import account from "../sdk/appwrite.jsx";
 import { Link } from "react-router-dom";
-function Login() {
+function Auth() {
   const loginwithGoogle = async (e) => {
     e.preventDefault();
     const acc = account.createOAuth2Session(
@@ -13,8 +13,188 @@ function Login() {
   };
   return (
     <>
-      <div>
-        <button
+      <div
+        style={{
+          margin: 120,
+        }}
+      >
+        <section>
+          <center>
+            <p
+              style={{
+                fontFamily: "Poppins",
+                fontStyle: "normal",
+                fontWeight: 800,
+                color: "#030232",
+                fontSize: "30px",
+              }}
+            >
+              InterViewPet
+            </p>
+            <p
+              style={{
+                fontFamily: "Poppins",
+                marginTop: -20,
+                fontStyle: "italic",
+                fontWeight: 400,
+                fontSize: "20px",
+                color: "#030232",
+              }}
+            >
+              InterViewPet - A Seamless Digital InterView Platform
+            </p>
+          </center>
+
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "row",
+              flex: 1,
+            }}
+          >
+            <div
+              style={{
+                marginTop: 50,
+                flex: 0.3,
+                borderRadius: 10,
+                backgroundColor: "#030232",
+              }}
+            >
+              <center>
+                <p
+                  style={{
+                    fontFamily: "Poppins",
+                    fontStyle: "normal",
+                    fontWeight: 800,
+                    color: "white",
+                    fontSize: "30px",
+                  }}
+                >
+                  Authentication
+                </p>{" "}
+                <div
+                  style={{
+                    marginTop: 50,
+                    width: 400,
+                    margin: 30,
+                    borderRadius: 10,
+                    display: "flex",
+                    flexDirection: "column",
+                    flex: 1,
+                    backgroundColor: "white",
+                  }}
+                >
+                  <div
+                    style={{
+                      flex: 1,
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "flex-start",
+                      height: 70,
+                      verticalAlign: "center",
+                      borderRadius: 10,
+                    }}
+                  >
+                    <img
+                      src="../assets/google.png"
+                      style={{
+                        width: 50,
+                        height: 50,
+                        marginTop: 10,
+                        marginLeft: 30,
+                      }}
+                    />
+                    <p
+                      style={{
+                        fontFamily: "Poppins",
+                        fontStyle: "normal",
+                        color: "#030232",
+                        marginLeft: 20,
+                        fontWeight: 600,
+                        fontSize: "20px",
+                      }}
+                    >
+                      SignIn With Google
+                    </p>
+                  </div>
+                </div>
+                <div
+                  style={{
+                    marginTop: 50,
+                    width: 400,
+                    margin: 30,
+                    marginBottom: 50,
+                    borderRadius: 10,
+                    display: "flex",
+                    flexDirection: "column",
+                    flex: 1,
+                    backgroundColor: "white",
+                  }}
+                >
+                  <div
+                    style={{
+                      flex: 1,
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "flex-start",
+                      height: 70,
+                      verticalAlign: "center",
+                      borderRadius: 10,
+                    }}
+                  >
+                    <img
+                      src="../assets/google.png"
+                      style={{
+                        width: 50,
+                        height: 50,
+                        marginTop: 10,
+                        marginLeft: 30,
+                      }}
+                    />
+                    <p
+                      style={{
+                        fontFamily: "Poppins",
+                        fontStyle: "normal",
+                        color: "#030232",
+                        marginLeft: 20,
+                        fontWeight: 600,
+                        fontSize: "20px",
+                      }}
+                    >
+                      SignUp With Google
+                    </p>
+                  </div>
+                </div>
+              </center>
+            </div>
+            <div
+              style={{
+                flex: 0.5,
+                backgroundColor: "red",
+              }}
+            >
+              <center>
+                {" "}
+                <img
+                  style={{
+                    display: "flex",
+                    flex: 0.8,
+                    position: "fixed",
+                    width: "40%",
+                    right: "100px",
+                    top: "300px",
+                    height: "40%",
+                    overflow: "hidden",
+                  }}
+                  src="../assets/login.svg"
+                />{" "}
+              </center>{" "}
+            </div>
+          </div>
+        </section>
+        {/* <button
           onClick={(e) => loginwithGoogle(e)}
           style={{
             width: "100px",
@@ -28,9 +208,34 @@ function Login() {
         >
           {" "}
           Sign In
-        </button>
+        </button> */}
+
+        <div
+          style={{
+            display: "flex",
+            left: 0,
+            justifyContent: "center",
+            position: "absolute",
+            bottom: "0px",
+            backgroundColor: "#030232",
+            width: "100%",
+          }}
+        >
+          <p
+            style={{
+              fontFamily: "Poppins",
+
+              fontStyle: "italic",
+              fontWeight: 300,
+              fontSize: "15px",
+              color: "white",
+            }}
+          >
+            InterViewPet - A Seamless Digital InterView Platform
+          </p>
+        </div>
       </div>
     </>
   );
 }
-export default Login;
+export default Auth;
--- interviewpet/src/main.jsx
+++ interviewpet/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Login from "./components/login.jsx";
+import Auth from "./components/auth.jsx";
 import "./index.css";
 import Home from "./components/home.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <NavBar />
     <Routes>
-      <Route path="/login" element={<Login />} />
+      <Route path="/auth" element={<Auth />} />
       <Route path="/" element={<Landing />} />
       <Route path="/interview" element={<Interview />} />
     </Routes>
